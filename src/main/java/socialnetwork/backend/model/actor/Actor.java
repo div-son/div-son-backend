5de257d2428ab24c3b01e8aaea7e103bebd5fc0c
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-13 00:23:59 +0100

    fix

--- src/main/java/socialnetwork/backend/model/actor/Actor.java
+++ src/main/java/socialnetwork/backend/model/actor/Actor.java
@@ -3,10 +3,16 @@ package socialnetwork.backend.model.actor;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 
 public class Actor {
+
+    @Id
+    private String id;
+
+    private String email;
 }
