8553afb12389b954ab7589c99bbcd78be9daabf8
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-12 19:08:33 +0100

    created module

--- src/test/java/socialnetwork/backend/BackendApplicationTests.java
+++ src/test/java/socialnetwork/backend/BackendApplicationTests.java
@@ -2,6 +2,7 @@ package socialnetwork.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
