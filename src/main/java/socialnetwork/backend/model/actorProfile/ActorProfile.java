1fdceba5387fcc81e7ea17cdf97eb1e38828b5d1
ZipDemon <okoroaforkelechi123@gmail.com>
2022-02-13 13:25:43 +0100

    Merge remote-tracking branch 'origin/zip-demon'

--- src/main/java/socialnetwork/backend/model/actorProfile/ActorProfile.java
+++ src/main/java/socialnetwork/backend/model/actorProfile/ActorProfile.java
@@ -3,10 +3,10 @@ package socialnetwork.backend.model.actorProfile;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 
 public class ActorProfile {
 }
