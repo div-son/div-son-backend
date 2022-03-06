9764e266cf113374dbacb579584c8479c2f07233
ZipDemon <zipdemon123@gmail.com>
2022-03-06 20:45:20 +0100

    added

--- src/main/java/socialnetwork/backend/controller/message/MessageController.java
+++ src/main/java/socialnetwork/backend/controller/message/MessageController.java
@@ -1,14 +1,21 @@
 package socialnetwork.backend.controller.message;
 
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import socialnetwork.backend.dto.request.WriteMessageDto;
 import socialnetwork.backend.model.message.Message;
 import socialnetwork.backend.repository.message.MessageRepository;
 
+import javax.validation.Valid;
+
 @RestController
-@RequestMapping("/api/send-receive/messages")
+@RequestMapping("/api/messages")
 @Slf4j
 
 public class MessageController {
@@ -18,6 +25,12 @@ public class MessageController {
     @Autowired
     MessageRepository messageRepository;
 
+    ModelMapper modelMapper = new ModelMapper();
 
+
+    @PostMapping("/send")
+    public ResponseEntity<?> sendMessage(@Valid @RequestBody WriteMessageDto writeMessageDto){
+        if (writeMessageDto.get)
+    }
 
 }
