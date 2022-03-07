a727f093963dbaacf1618399f7f4397b3b3ee661
ZipDemon <zipdemon123@gmail.com>
2022-03-07 17:14:42 +0100

    removed message controller

--- src/main/java/socialnetwork/backend/dto/request/RegisterVisitorDto.java
+++ src/main/java/socialnetwork/backend/dto/request/RegisterVisitorDto.java
@@ -20,8 +20,10 @@ public class RegisterVisitorDto {
     @NotEmpty(message = "First name is required")
     private String firstName;
 
+    @Size(min = 2, max = 20, message = "Last name should be 2 to 20 character long")
     private String lastName;
 
+    @Pattern(regexp = "^[@]\\s")
     private String email;
 
     private String password;
